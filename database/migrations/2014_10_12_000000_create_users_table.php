014bc186166d781a91c1e2c61bd7af1693ada1a4
vipos89 <1989146ViPoS>
2022-03-23 16:44:56 +0400

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
+
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -21,6 +23,14 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+
+
+
+
+
+
+
         });
     }
 
