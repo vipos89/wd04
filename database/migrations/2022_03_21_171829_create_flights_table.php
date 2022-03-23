1087c9b5e98ae59c0f2299442ce8338799aa23c9
vipos89 <1989146ViPoS>
2022-03-23 16:45:15 +0400

    init

---
+++ database/migrations/2022_03_21_171829_create_flights_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+
+            $table->smallInteger('votes')
+                ->unsigned()
+                ->default(5)
+                ->nullable(false);
+
+            $table->string('flight_name', '255');
+            $table->string('ololo', '255');
+            $table->string('email', '255')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
