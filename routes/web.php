6008541018cdee87494e216d1d5a16191d907499
vipos89 <1989146ViPoS>
2022-03-28 22:46:53 +0400

    login and auth

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::get('my-first-page', [\App\Http\Controllers\MyController::class, 'myPage']);
 
 Auth::routes();
-Route::get('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+//Route::get('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
 //
 //Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
 //Route::post('auth', [\App\Http\Controllers\AuthController::class, 'auth']);
