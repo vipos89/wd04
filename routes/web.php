b8f671aa26040ec13a2c628ebacbec93041cbc45
vipos89 <1989146ViPoS>
2022-03-30 20:41:56 +0400

    admin template

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('ololo', [\App\Http\Controllers\MyController::class, 'ololo']);
+Route::get('ololo', function (){
+    return view('layouts.admin');
+});
 
 Route::get('/', function () {
     return view('welcome');
